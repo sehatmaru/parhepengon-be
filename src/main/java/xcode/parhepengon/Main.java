952a7db2fe781e4bae9c9aa1c8bfe00689b1ad73
sehatmaru <sehat.samosir@adapundi.com>
2023-06-09 17:32:17 +0700

    init project

---
+++ src/main/java/xcode/parhepengon/Main.java
@@ -0,0 +1,16 @@
+package xcode.parhepengon;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableEncryptableProperties
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
